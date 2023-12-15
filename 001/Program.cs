28bb3484cdefc61fd6b23bb63c5a19f47a5d91c6
Pavel <jinkofficially@gmail.com>
2023-12-15 19:04:30 +0300

    complited work

--- 001/Program.cs
+++ 001/Program.cs
@@ -6,3 +6,44 @@
 //[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 //[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 //[“Russia”, “Denmark”, “Kazan”] → []
+
+string[] number = Print("Введите элементы массива через запятую и пробел: ");
+string[] result = Filter(number);
+
+Console.WriteLine("Результат:");
+for (int i = 0; i < result.Length; i++)
+{
+    Console.Write($"{result[i]} ");
+}
+
+
+static string[] Print(string text)
+{
+    Console.Write(text);
+    return Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.None);
+}
+
+static string[] Filter(string[] number)
+{
+    int count = 0;
+    for (int i = 0; i < number.Length; i++)
+    {
+        if (number[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    string[] result = new string[count];
+    int ind = 0;
+    for (int i = 0; i < number.Length; i++)
+    {
+        if (number[i].Length <= 3)
+        {
+            result[ind] = number[i];
+            ind++;
+        }
+    }
+
+    return result;
+}
